dded380c5ca42622743b347fc7e21d0e3efea5a3
uevitondev <uevitonsantos@outlook.com>
2024-04-30 13:32:35 -0300

    project created

---
+++ src/main/java/com/uevitondev/deliverybackend/DeliverybackendApplication.java
@@ -0,0 +1,13 @@
+package com.uevitondev.deliverybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uevitondev/deliverybackend/DeliverybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uevitondev.deliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
