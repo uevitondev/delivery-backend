b2a9865eff6ae32269be86e56964972a87bdfecc
uevitondev <uevitonsantos@outlook.com>
2024-05-07 15:30:17 -0300

    feat: add api doc (openapi)

--- src/main/java/com/uevitondev/deliverybackend/security/config/WebSecurityConfig.java
+++ src/main/java/com/uevitondev/deliverybackend/security/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig {
     private final JwtService jwtService;
     private final HandlerExceptionResolver resolver;
 
-    private static final String[] ENDPOINTS_PUBLIC = {"/delivery/v1/h2-console/**", "/delivery/v1/api/test/public/**", "/delivery/v1/api/auth/sign-in/**"};
+    private static final String[] ENDPOINTS_PUBLIC = {"/delivery/v1/h2-console/**", "/delivery/v1/doc/**", "/delivery/v1/api/test/public/**", "/delivery/v1/api/auth/sign-in/**"};
     private static final String[] ENDPOINTS_ADMIN = {"/delivery/v1/api/admin/**"};
 
 
